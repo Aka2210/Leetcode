cbe0a7784f6733d432d988e0fdeee167b51bc2f2
Aka2210 <qoo0973215128@gmail.com>
2023-08-13 19:45:37 +0800

    Finish 27. Remove Element

--- 27. Remove Element/LeetCode27Ex.cpp
+++ 27. Remove Element/LeetCode27Ex.cpp
@@ -6,18 +6,18 @@ using namespace std;
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        int k = 0;
-
-        for(int i = 0; i < nums.size(); i++)
+        int n=nums.size();
+        int count=0;
+        for(int i=0;i<n;i++)
         {
-            if(nums[i] != val)
+            if(nums[i]!=val)
             {
-                nums[k] = nums[i];
-                k += 1;
+                swap(nums[i],nums[count]);
+                count++;
             }
         }
-
-        return k;
+        
+        return count;
     }
 };
 

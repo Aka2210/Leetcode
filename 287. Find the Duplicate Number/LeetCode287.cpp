1669a668526501e40fa4d807c2ecea5625df478d
Aka2210 <qoo0973215128@gmail.com>
2023-09-13 17:27:17 +0800

    Finish 287. Find the Duplicate Number

---
+++ 287. Find the Duplicate Number/LeetCode287.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int slow = nums[0];
+        int fast = nums[nums[0]];
+        
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
+        }
+        
+        slow = 0;
+        while (slow != fast) {
+            slow = nums[slow];
+            fast = nums[fast];
+        }
+        return slow;
+    }
+};
+
+int main()
+{
+    vector<int>nums;
+
+    int question;
+
+    while(cin >> question)
+        nums.push_back(question);
+
+    Solution AnswerFunction;
+
+    cout << AnswerFunction.findDuplicate(nums);
+}

f243f77a7895b18a69588b98cfc5bb3f100bc8e4
Aka2210 <qoo0973215128@gmail.com>
2023-09-14 00:37:26 +0800

    Finish 18. 4Sum

--- 18. 4Sum/LeetCode18.cpp
+++ 18. 4Sum/LeetCode18.cpp
@@ -5,68 +5,52 @@
 
 using namespace std;
 
-// 寫不出Two Point解法。
-
 class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
-        int j = nums.size() - 1, index = 0;
-
+        sort(nums.begin(), nums.end());
+        int i = 0, j, k, h = nums.size() - 1, n = nums.size();
         vector<vector<int>> ans;
 
-        sort(nums.begin(), nums.end());
-
-        while(index < j)
+        for(i = 0; i < n - 3; i++)
         {
-            int i = index + 1, k = j - 1, initSum = nums[k] + nums[index] + nums[i] + nums[j];
-
-            while(i < k)
+            if (i > 0 && nums[i] == nums[i - 1])
+                continue;
+            for(j = i + 1; j < n - 2; j++)
             {
-                int sum = nums[k] + nums[index] + nums[i] + nums[j];
-                if(sum > target)
+                if(j > i + 1 && nums[j] == nums[j - 1])
+                    continue;
+
+                k = j + 1;
+                h = n - 1;
+
+                while(k < h)
                 {
-                    k--;
+                    long int sum = nums[i] + nums[j] + nums[k] + nums[h];
 
-                    while(nums[k - 1] == nums[k] && i < k)
-                        k--;
+                    if(sum > target)
+                    {
+                        while(h - 1 >= 0 && nums[h] == nums[h - 1])
+                            h--;
+                        h--;//-2 -1 0 0 1 2
+                    }
+                    else if(sum < target)
+                    {
+                        while(k + 1 < nums.size() && nums[k] == nums[k + 1])
+                            k++;
+                        k++;
+                    }
+                    else
+                    {
+                        ans.push_back({nums[i], nums[j], nums[k], nums[h]});
+                        while(k + 1 < nums.size() && nums[k] == nums[k + 1])
+                            k++;
+                        k++;
+                        while(h - 1 >= 0 && nums[h] == nums[h - 1])
+                            h--;
+                        h--;
+                    }
                 }
-                else if(sum < target)
-                {
-                    i++;
-
-                    while(nums[i - 1] == nums[i] && i < k)
-                        i++;
-                }
-                else
-                {
-                    ans.push_back({nums[i], nums[j], nums[k], nums[index]});
-                    k--;
-                    i++;
-                }
-            }
-
-            if(initSum == target)
-            {
-                j--;
-                index++;
-
-                while(nums[index - 1] == nums[index] && index < j)
-                    index++;
-                
-                while(nums[j + 1] == nums[j] && index < j)
-                    j--;
-            }
-            else if(initSum > target)
-            {
-                j--;
-                while(nums[j + 1] == nums[j] && index < j)
-                    j--;
-            }
-            else
-            {
-                index++;
-                while(nums[index - 1] == nums[index] && index < j)
-                    index++;
             }
         }
 
@@ -93,6 +77,4 @@ int main()
             cout << num << " ";
         cout << endl;
     }
-
-    cout << "Finish";
 }

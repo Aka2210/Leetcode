a74d120ecaf4e4f27929001c8383df5244f0dc3e
Aka2210 <qoo0973215128@gmail.com>
2023-09-18 19:03:35 +0800

    Finish 287. Find the Duplicate Number

--- 287. Find the Duplicate Number/LeetCode287.cpp
+++ 287. Find the Duplicate Number/LeetCode287.cpp
@@ -8,19 +8,22 @@ using namespace std;
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
-        int slow = nums[0];
-        int fast = nums[nums[0]];
-        
-        while (slow != fast) {
-            slow = nums[slow];  //移動一步
-            fast = nums[nums[fast]]; //移動兩步
+        int slow = nums[0], fast = nums[nums[slow]];
+
+        while(slow != fast)
+        {
+            slow = nums[slow];
+            fast = nums[nums[fast]];
         }
-        
+
         slow = 0;
-        while (slow != fast) {
+
+        while(slow != fast)
+        {
             slow = nums[slow];
             fast = nums[fast];
         }
+
         return slow;
     }
 };

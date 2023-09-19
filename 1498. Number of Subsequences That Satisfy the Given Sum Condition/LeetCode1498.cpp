a99cfdffd4d7844f0fd63949d67d528627a346fd
Aka2210 <qoo0973215128@gmail.com>
2023-09-19 20:15:10 +0800

    Finish 1498.

--- 1498. Number of Subsequences That Satisfy the Given Sum Condition/LeetCode1498.cpp
+++ 1498. Number of Subsequences That Satisfy the Given Sum Condition/LeetCode1498.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 #include<vector>
-#include<unordered_set>
+#include<math.h>
 #include <algorithm>
 
 using namespace std;
@@ -8,7 +8,32 @@ using namespace std;
 class Solution {
 public:
     int numSubseq(vector<int>& nums, int target) {
+        sort(nums.begin(), nums.end());
         
+        int L = 0, R = nums.size() - 1;
+        int mod = 1e9+7;
+        long long ans = fastPower(2, nums.size(), mod) - 1;
+        
+        while (L <= R) {
+            if (nums[L] + nums[R] <= target) {
+                L++;
+            } else {
+                ans = (ans - fastPower(2, R - L, mod) + mod) % mod;
+                R--;
+            }
+        }
+        
+        return ans;
+    }
+    
+    long long fastPower(long long base, int exp, int mod) {
+        long long result = 1;
+        while (exp) {
+            if (exp & 1) result = (result * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+        return result;
     }
 };
 

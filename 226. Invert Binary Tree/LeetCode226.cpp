6ce2c7144566282453e1d95c75f5bc3443000ce9
Aka2210 <qoo0973215128@gmail.com>
2023-09-23 00:09:03 +0800

    Finish 226. Invert Binary Tree

---
+++ 226. Invert Binary Tree/LeetCode226.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    TreeNode* invertTree(TreeNode* root) {
+        if(root->left != NULL || root->right != NULL)
+        {
+            TreeNode* temp = root->left;
+            root->left = root->right;
+            root->right = temp;
+            invertTree(root->left);
+            invertTree(root->right);
+        }
+        else
+            return;
+
+        return root;
+    }
+};

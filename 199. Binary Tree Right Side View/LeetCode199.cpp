eda1e4f8f4880f84dde462b9f5422d0f6d53c0f6
Aka2210 <qoo0973215128@gmail.com>
2023-09-26 03:43:23 +0800

    Finish 199. Binary Tree Right Side View

---
+++ 199. Binary Tree Right Side View/LeetCode199.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<stack>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if(root == NULL)
+            return {};
+
+        vector<int> ans;
+        queue<TreeNode*> q;
+        
+        q.push(root);
+
+        while(!q.empty())
+        {
+            int count = q.size();
+            ans.push_back(q.back()->val);
+
+            for(int i = 0; i < count; i++)
+            {
+                if(q.front()->left != NULL)
+                    q.push(q.front()->left);
+                
+                if(q.front()->right != NULL)
+                    q.push(q.front()->right);
+                
+                q.pop();
+            }
+        }
+        return ans;
+    }
+};

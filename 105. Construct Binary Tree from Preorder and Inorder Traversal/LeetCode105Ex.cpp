907158e1e2279711565aaa67fb5083c92e212a8d
Aka2210 <qoo0973215128@gmail.com>
2023-09-27 22:25:05 +0800

    Finish 105.

--- 105. Construct Binary Tree from Preorder and Inorder Traversal/LeetCode105Ex.cpp
+++ 105. Construct Binary Tree from Preorder and Inorder Traversal/LeetCode105Ex.cpp
@@ -19,28 +19,28 @@ class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
         int index = 0;
-        return dfs(preorder, inorder, index, 0, inorder.size() - 1);
+        return build(preorder, inorder, index, 0, inorder.size() - 1);
     }
-
 private:
-    TreeNode* dfs(vector<int>& preorder, vector<int>& inorder, int& index, int Left, int Right)
-    {
-        if(Left > Right)
+    TreeNode* build(vector<int>& preorder, vector<int>& inorder, int& index, int i, int j) {
+        if (i > j) {
             return NULL;
-
-        TreeNode* root = new TreeNode(preorder[index]);
-
-        int split = Left;
-        for(; split <= Right; split++)
-        {
-            if(preorder[index] == inorder[split])
-                break;
         }
-
+        
+        TreeNode* root = new TreeNode(preorder[index]);
+        
+        int split = 0;
+        for (int i = 0; i < inorder.size(); i++) {
+            if (preorder[index] == inorder[i]) {
+                split = i;
+                break;
+            }
+        }
         index++;
-        root->left = dfs(preorder, inorder, index, Left, split - 1);
-        root->right = dfs(preorder, inorder, index, split + 1, Right);
-
+        
+        root->left = build(preorder, inorder, index, i, split - 1);
+        root->right = build(preorder, inorder, index, split + 1, j);
+        
         return root;
     }
 };

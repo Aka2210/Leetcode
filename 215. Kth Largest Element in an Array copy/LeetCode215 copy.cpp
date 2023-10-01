77cb45d197c67ffd3880e3c870a1e5fb85e8ae86
Aka2210 <qoo0973215128@gmail.com>
2023-10-02 02:09:20 +0800

    Finish 215. Kth Largest Element in an Array copy

---
+++ 215. Kth Largest Element in an Array copy/LeetCode215 copy.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<algorithm>
+
+using namespace std;
+
+class Solution {
+private:
+    static bool st(string &a,string &b){
+        if(a.size()==b.size()) return a<b;
+        return a.size()<b.size();
+    }
+    
+    
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        sort(nums.begin(),nums.end(),st);
+        return nums[nums.size()-k];
+        
+    }
+};
---
+++ 215. Kth Largest Element in an Array copy/LeetCode215.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<queue>
+#include<math.h>
+
+using namespace std;
+
+struct CompareByLength {
+    bool operator()(string& a, string& b) {
+        if(a.size() != b.size())
+            return a.size() < b.size();
+        else
+            return a < b;
+    }
+};
+
+class Solution {
+public:
+    string kthLargestNumber(vector<string>& nums, int k) {
+        priority_queue<string, vector<string>, CompareByLength> pq(nums.begin(), nums.end());
+
+        for(k--; k > 0; k--)
+            pq.pop();
+        
+        return pq.top();
+    }
+};

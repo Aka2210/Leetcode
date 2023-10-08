416720a054ce5769685657a7d1ffec9a98db45fc
Aka2210 <qoo0973215128@gmail.com>
2023-10-09 07:26:39 +0800

    Finish 846. Hand of Straights

--- 846. Hand of Straights/LeetCode846.cpp
+++ 846. Hand of Straights/LeetCode846.cpp
@@ -1,8 +1,8 @@
 #include<iostream>
-#include<string>
-#include<vector>
 #include<queue>
-#include<math.h>
+#include<vector>
+#include<algorithm>
+#include<map>
 
 using namespace std;
 
@@ -11,7 +11,30 @@ public:
     bool isNStraightHand(vector<int>& hand, int groupSize) {
         if(hand.size() % groupSize != 0)
             return false;
+        else if(groupSize == 1)
+            return true;
 
-        
+        map<int, int> records;
+        int result = 0;
+
+        for(int i = 0; i < hand.size(); i++)
+            records[hand[i]]++;
+
+        while(!records.empty())
+        {
+            int curr = records.begin()->first;
+            for(int i = 0; i < groupSize; i++)
+            {
+                if(records[curr+i] == 0)
+                    return false;
+                else
+                    records[curr+i]--;
+                
+                if(records[curr+i] == 0)
+                    records.erase(curr+i);
+            }
+        }
+
+        return true;
     }
 };

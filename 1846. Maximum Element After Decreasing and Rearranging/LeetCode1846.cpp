7a60207f110da788321ffb65d969128ac1fc220d
Aka2210 <qoo0973215128@gmail.com>
2023-11-16 12:34:49 +0800

    Debug LeetCode1846

--- 1846. Maximum Element After Decreasing and Rearranging/LeetCode1846.cpp
+++ 1846. Maximum Element After Decreasing and Rearranging/LeetCode1846.cpp
@@ -6,7 +6,7 @@
 #include<algorithm>
 
 using namespace std;
-
+//若陣列長度不大，此算法即可
 class Solution {
 public:
     int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
@@ -22,3 +22,37 @@ public:
         return arr.back();
     }
 };
+
+//陣列長度大算法如下:
+/*
+class Solution {
+public:
+    int maximumElementAfterDecrementingAndRearranging(vector<int>& arr) {
+        unordered_map<int, int> ValRecords;
+
+        for(int i = 0; i < arr.size(); i++)
+        {
+            if(arr[i] <= arr.size())
+                ValRecords[arr[i]]++;
+            else
+                ValRecords[arr.size()]++;
+        }
+
+        int result = 1, offset = 0, Val = 1;
+        for(int i = 0; i < arr.size(); i++, Val++)
+        {     
+            while(ValRecords[Val] != 0)
+            {
+                ValRecords[Val]--;
+                if(result < Val)
+                    result++;
+                i++;
+            }
+
+            i--;
+        }
+
+        return result;
+    }
+};
+*/
---
+++ 684. Redundant Connection/LeetCode684.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        
+    }
+};

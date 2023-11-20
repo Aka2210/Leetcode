f5b7b8e16af835861bff0343f7bb53ab90bfb586
Aka2210 <qoo0973215128@gmail.com>
2023-11-20 18:12:13 +0800

    Finish LeetCode1584

---
+++ 1584. Min Cost to Connect All Points/LeetCode1584.cpp
@@ -0,0 +1,70 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<string.h>
+
+using namespace std;
+
+//Kruskal演算法做最小生成樹
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        int root[n], height[n];
+        for(int i = 0; i < n; i++)
+        {
+            root[i] = i;
+            height[i] = 0;
+            for(int j = i; j < n; j++)
+            {
+                int cost = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]);
+                int scoure = i, dict = j;
+                pq.push({cost, scoure, dict});
+            }
+        }
+        
+        int cost = 0, pqSize = pq.size(), edge = 0;
+        while(edge != n - 1)
+        {
+            int CurrCost = pq.top()[0], scoure = pq.top()[1], dict = pq.top()[2]; 
+            if(Union(scoure, dict, root, height))
+            {
+                cost += CurrCost;
+                edge++;
+            }
+            pq.pop();
+        }
+
+        return cost;
+    }
+private:
+    int Find(int val, int* root)
+    {
+        if(root[val] != val)
+            root[val] = Find(root[val], root);
+        return root[val];
+    }
+
+    bool Union(int a, int b, int* root, int* height)
+    {
+        a = Find(a, root);
+        b = Find(b, root);
+        if(a == b)
+            return false;
+        
+        if(height[a] > height[b])
+            root[b] = a;
+        else if(height[a] < height[b])
+            root[a] = b;
+        else
+        {
+            root[b] = a;
+            height[a]++;
+        }
+
+        return true;
+    }
+};
---
+++ 1584. Min Cost to Connect All Points/LeetCode1584Ex.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<string>
+#include<vector>
+#include<unordered_map>
+#include<unordered_set>
+#include<string.h>
+
+using namespace std;
+
+//Prim演算法做最小生成樹
+class Solution {
+public:
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n = points.size();
+        
+        int edgesUsed = 0;
+        // track visited nodes
+        vector<bool> inMST(n);
+        vector<int> minDist(n, INT_MAX);
+        minDist[0] = 0;
+        
+        int result = 0;
+        
+        while (edgesUsed < n) {
+            int currMinEdge = INT_MAX;
+            int currNode = -1;
+            
+            // greedily pick lowest cost node not in MST
+            for (int i = 0; i < n; i++) {
+                if (!inMST[i] && currMinEdge > minDist[i]) {
+                    currMinEdge = minDist[i];
+                    currNode = i;
+                }
+            }
+            
+            result += currMinEdge;
+            edgesUsed++;
+            inMST[currNode] = true;
+            
+            // update adj nodes of curr node
+            for (int i = 0; i < n; i++) {
+                int cost = abs(points[currNode][0] - points[i][0])
+                    + abs(points[currNode][1] - points[i][1]);
+                
+                if (!inMST[i] && minDist[i] > cost) {
+                    minDist[i] = cost;
+                }
+            }
+        }
+        
+        return result;
+    }
+};

35dc6799c2565e6316e6ad553bad752177fd844d
Aka2210 <qoo0973215128@gmail.com>
2024-02-11 17:56:23 +0800

    Finish 322. Coin Change

---
+++ 322. Coin Change/LeetCode322.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<unordered_set>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<int> dp;
+        dp.push_back(0);
+
+        for(int i = 1; i <= amount; i++)
+        {
+            dp.push_back(INT32_MAX);
+            for(int j = 0; j < n; j++)
+            {
+                int Find = i - coins[j];
+                if(Find < 0) continue;
+                else if(Find == 0)
+                {
+                    dp[i] = 1;
+                    break;
+                }
+                else if(dp[Find] != -1) dp[i] = min(dp[i], dp[Find] + 1);
+            }
+
+            if(dp[i] == INT32_MAX)
+                dp[i] = -1;
+        }
+
+        return dp[amount];
+    }
+};

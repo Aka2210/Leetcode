c8ae7b3ca8cf3147e4f263d00f17032edb840a2d
Aka2210 <qoo0973215128@gmail.com>
2024-10-20 08:02:47 +0800

    Modify

--- 1/sender.c
+++ 1/sender.c
@@ -15,7 +15,7 @@ void send(message_t message, mailbox_t* mailbox_ptr)
         if(mailbox_ptr->flag == 2)
         {
             clock_gettime(CLOCK_MONOTONIC, &start);
-            message = (message_t) shmat(mailbox_ptr->storage.msqid, (void*)0, 0);
+            message = *((message_t*) shmat(mailbox_ptr->storage.msqid, (void*)0, 0));
             clock_gettime(CLOCK_MONOTONIC, &end);
             time_taken += (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) * 1e-9;
         }

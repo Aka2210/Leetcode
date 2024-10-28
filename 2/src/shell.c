1cada57ff4977e64da008db501e0a5d35e738478
Aka2210 <qoo0973215128@gmail.com>
2024-10-28 23:01:50 +0800

    test fork cmd node

--- 2/src/shell.c
+++ 2/src/shell.c
@@ -105,7 +105,7 @@ int fork_cmd_node(struct cmd *cmd)
 	int pipe_prv = 0;
 	while(cmd->head != NULL)
 	{
-		int pipe_fd[2] = NULL;
+		int pipe_fd[2];
 		if(cmd->head->next != NULL)
 		{
 			pipe(pipe_fd);
